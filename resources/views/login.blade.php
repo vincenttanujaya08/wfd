43984db5051be41e6ec7c22163cecde858e04d98
AxelValentP <c14230145@john.petra.ac.id>
2024-11-30 12:38:44 +0700

    start awal

---
+++ resources/views/login.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div  style="text-align: center; color:chocolate; background-color:beige">
+        <h1><Strong>Welcome to Todit</Strong></h1>
+        <p>Share your moments here</p>
+        <p>Interact with over <strong>1 million</strong> users <strong>Globaly</strong></p>
+    </div>
+</body>
+</html>
