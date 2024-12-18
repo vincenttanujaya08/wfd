433e446d5c9206e35d3040be86cbe8e4f9227879
AxelValentP <c14230145@john.petra.ac.id>
2024-12-18 16:44:52 +0700

    coment
    comment page

--- routes/web.php
+++ routes/web.php
@@ -15,12 +15,44 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('home');
 });
+<<<<<<< Updated upstream
 Route::get('/login', function () {
     return view('login');
 });
 Route::get('/signup', function () {
     return view('signup');
 });
+=======
+
+
+
+// Protected Route - Only accessible if authenticated
+Route::get('/app', function () {
+    return view('app');
+})->name('app')->middleware('auth');
+
+Route::get('/comment', function () {
+    return view('comment');
+})->name('comment')->middleware('auth');
+
+
+
+// Login Route - Only accessible if not authenticated (using guest middleware)
+Route::get('login', [LoginController::class, 'showLoginForm'])->name('login');
+Route::post('login', [LoginController::class, 'login']);
+Route::post('logout', [LoginController::class, 'logout'])->name('logout');
+
+use App\Http\Controllers\SignupController;
+
+Route::get('/signup', [SignupController::class, 'showSignup'])->name('signup'); 
+Route::post('/signup', [SignupController::class, 'processSignup'])->middleware('guest'); 
+
+
+
+
+
+// Database test route (for checking DB connection)
+>>>>>>> Stashed changes
 use Illuminate\Support\Facades\DB;
 
     Route::get('/db-test', function () {
