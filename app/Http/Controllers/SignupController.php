de7266bffdfe0157f46f48147bb62fa1c3cb5bb5
timothyteguh1 <c14230133@john.petra.ac.id>
2024-12-19 01:24:22 +0700

    upload images

--- app/Http/Controllers/SignupController.php
+++ app/Http/Controllers/SignupController.php
@@ -39,6 +39,6 @@ public function processSignup(Request $request)
     Auth::login($user);
 
     // Redirect to the app page after successful registration
-    return redirect()->route('app');
+    return redirect()->route('explore');
 }
 }
