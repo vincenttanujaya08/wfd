add88c935d4084ca2698539036ccac022c85eb0a
timothyteguh1 <c14230133@john.petra.ac.id>
2024-12-19 12:43:24 +0700

    update link adrres image

--- database/migrations/2024_12_18_160150_create_images_table.php
+++ database/migrations/2024_12_18_160150_create_images_table.php
@@ -11,7 +11,7 @@ return new class extends Migration
         Schema::create('images', function (Blueprint $table) {
             $table->id();
             $table->foreignId('post_id')->constrained()->onDelete('cascade');
-            $table->string('path'); // Path ke gambar
+            $table->longText('path'); // Path ke gambar
             $table->timestamps();
         });
     }
